58847f2b544e08155e510e8853ba2a2e2ada44ee
charozoid <directfromquebec@gmail.com>
2023-11-20 13:33:18 -0500

    Removed sin function and added grid

--- 2DIsometricViewer/Program.cs
+++ 2DIsometricViewer/Program.cs
@@ -7,12 +7,12 @@ class Program
     public const int HEIGHT = 600;
     public static VideoMode mode = new VideoMode(WIDTH, HEIGHT);
     public static RenderWindow window = new RenderWindow(mode, "Isometric");
-    public static Texture cubeText = new Texture("../../Assets/cube.png");
+    public static Texture cubeText = new Texture("../../Assets/spritesheet.png");
     public static Tile[,] tiles = new Tile[32,32];
     public static View view = new View(new FloatRect(0, 0, WIDTH, HEIGHT));
     public static float CameraSpeed = 10.0f;
     public static float ZoomFactor = 1.05f;
-    public static float amplitude = 0.5f;
+    public static float amplitude = 0.25f;
     public static float frequency = 2f;
     public static Random random = new Random();
     public static Clock clock = new Clock();
@@ -22,48 +22,31 @@ class Program
         window.SetVerticalSyncEnabled(true);
         window.Closed += (sender, args) => window.Close();
 
-        for (int i = 0; i < 32; i++)
-        {
-            for (int j = 0; j < 32; j++)
-            {
-                tiles[i, j] = new Tile(i, j);
-                tiles[i, j].sprite.Position = TransformVector(i, j);
-            }
-        }
-
-        //tiles[10, 10].sprite.Position = new Vector2f(tiles[10, 10].sprite.Position.X, tiles[10, 10].sprite.Position.Y - 25);
-        //tiles[11, 10].sprite.Position = new Vector2f(tiles[11, 10].sprite.Position.X, tiles[11, 10].sprite.Position.Y - 50);
         while (window.IsOpen)
         {
-            float deltaTime = clock.Restart().AsSeconds();
             window.SetView(view);
             window.Clear();
-
-            for (int i = 0; i < 32; i++)
-            {
-                for (int j = 0; j < 32; j++)
-                {
-
-                    double seconds = DateTime.Now.Second;
-                    double phaseShift = frequency * (0.3 * i + 0.3 * j);
-                    double shiftedSeconds = seconds - phaseShift;
-                    double sinValue = Math.Sin(shiftedSeconds);
-                    Tile tile = tiles[i, j];
-                    Vector2f pos = tile.sprite.Position;
-                    float baseLine = pos.Y;
-                    pos = new Vector2f(pos.X, baseLine + amplitude * (float)sinValue);
-                    tile.sprite.Position = pos;
-                    window.Draw(tiles[i,j].sprite);
-
-                }
-            }
+            DrawGrid();
             KeyPressed();
 
             window.DispatchEvents();
             window.Display();
         }
     }
-
+    public static void DrawGrid()
+    {
+        int offset = 512;
+        Sprite sprite = new Sprite(cubeText);
+        sprite.TextureRect = new IntRect(64, 0, 32, 32);
+        for (int i = 0; i < 32; i++)
+        {
+            for (int j = 0; j < 32; j++)
+            {
+                sprite.Position = TransformVector(i, j);
+                window.Draw(sprite);
+            }
+        }
+    }
     public static void KeyPressed()
     {
         if (Keyboard.IsKeyPressed(Keyboard.Key.Up))
@@ -94,10 +77,10 @@ class Program
 
     public static Vector2f TransformVector(int x, int y)
     {
-        float newx = (x * 64) + (y * -64);
-        float newy = (x * 32) + (y * 32);
+        float newx = (x * 16) + (y * -16);
+        float newy = (x * 8) + (y * 8);
 
-        return new Vector2f(newx, newy);
+        return new Vector2f(newx + 496, newy);
     }
 }
 
@@ -118,5 +101,6 @@ class Tile
     {
         this.x = x;
         this.y = y;
+        sprite.TextureRect = new IntRect(0, 0, 32, 32);
     }
 }
