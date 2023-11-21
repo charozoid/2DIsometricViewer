34cbe1b29b6b4d03d431433ca29deb6a3f0748a2
charozoid <directfromquebec@gmail.com>
2023-11-20 23:24:35 -0500

    Fixed screen coordinates to isometric coordinates with zoom and screen movement

--- 2DIsometricViewer/Program.cs
+++ 2DIsometricViewer/Program.cs
@@ -8,45 +8,59 @@ class Program
     public static VideoMode mode = new VideoMode(WIDTH, HEIGHT);
     public static RenderWindow window = new RenderWindow(mode, "Isometric");
     public static Texture cubeText = new Texture("../../Assets/spritesheet.png");
-    public static Tile[,] tiles = new Tile[32,32];
+    public static Block[,,] blocks = new Block[32, 32, 8];
     public static View view = new View(new FloatRect(0, 0, WIDTH, HEIGHT));
+    public static View ui = new View(new FloatRect(0, 0, WIDTH, HEIGHT));
     public static Font font = new Font("../../Assets/Fonts/arialbd.ttf");
-    public static float CameraSpeed = 10.0f;
-    public static float ZoomFactor = 1.05f;
+    public static float CameraSpeed = 2.0f;
+    public static float ZoomFactor = 1.01f;
     public static float amplitude = 0.25f;
     public static float frequency = 2f;
+    public static int chosenElevation = 0;
     public static Random random = new Random();
     public static Clock clock = new Clock();
     public static List<Sprite> sprites = new List<Sprite>();
+    public static Dictionary<Block.Type, IntRect> spritePos = new Dictionary<Block.Type, IntRect>();
     public static void Main(string[] args)
     {
-
+        Clock keyPressedClock = new Clock();
+        spritePos[Block.Type.Block] = new IntRect(0, 0, 32, 32);
+        spritePos[Block.Type.Water] = new IntRect(0, 32, 32, 32);
         window.SetVerticalSyncEnabled(true);
         window.Closed += (sender, args) => window.Close();
 
+
+
         while (window.IsOpen)
         {
+
+
             window.SetView(view);
             window.Clear();
+            DrawBlockOnCursor();
             DrawGrid();
-
-            Sprite sprite = new Sprite(cubeText);
-            sprite.TextureRect = new IntRect(0, 0, 32, 32);
+            DrawBlocks();
+            foreach (Sprite sprite in sprites)
+            {
+                window.Draw(sprite);
+            }
+            window.SetView(ui);
             Vector2i mousePos = Mouse.GetPosition(window);
             Vector2f worldCoords = window.MapPixelToCoords(mousePos, view);
-            Vector2f gridCoords = GetMouseGrid(worldCoords, view, GetZoomFactor(view));
-            //Vector2f mousePosF = GetMouseGrid(new Vector2f(mousePos.X, mousePos.Y), view, GetZoomFactor(view));
-            sprite.Position = TransformVector(gridCoords);
-
+            Vector2f gridCoords = GetMouseGrid(worldCoords, view);
             Text text = new Text($"X: {gridCoords.X} Y: {gridCoords.Y}", font);
-            text.Position = new Vector2f(15, 50);
+            text.Position = new Vector2f(0, 0);
             window.Draw(text);
-            KeyPressed();
-            foreach (Sprite tile in sprites)
+            Text elevationText = new Text($"Elevation: {chosenElevation}", font);
+            elevationText.Position = new Vector2f(0, 20);
+            window.Draw(elevationText);
+            if (keyPressedClock.ElapsedTime.AsSeconds() >= 0.2f)
             {
-                window.Draw(tile);
+                KeyPressedDelay();
+                keyPressedClock.Restart();
+
             }
-            window.Draw(sprite);
+            KeyPressed();
             window.DispatchEvents();
             window.Display();
         }
@@ -59,17 +73,62 @@ class Program
         {
             for (int j = 0; j < 32; j++)
             {
-                sprite.Position = TransformVector(new Vector2f(i + 32, j));                
+                sprite.Position = TransformVectorToIsometric(new Vector2f(i, j));
                 window.Draw(sprite);
             }
         }
     }
-    public static void CreateBlock(Vector2f position)
+    public static void DrawBlocks()
+    {
+        for (int k = 0; k < 8; k++)
+        {
+            for (int i = 0; i < 32; i++)
+            {
+                for (int j = 0; j < 32; j++)
+                {
+                    Block block = blocks[i, j, k];
+                    if (blocks[i, j, k] != null && block.z == k)
+                    {
+                        window.Draw(blocks[i, j, k].sprite);
+                    }
+                }
+            }
+        }
+
+    }
+    public static void DrawBlockOnCursor()
     {
         Sprite sprite = new Sprite(cubeText);
         sprite.TextureRect = new IntRect(0, 0, 32, 32);
-        sprite.Position = TransformVector(position);
-        sprites.Add(sprite);
+        Vector2i mousePos = Mouse.GetPosition(window);
+        Vector2f worldCoords = window.MapPixelToCoords(mousePos, view);
+        Vector2f gridCoords = GetMouseGrid(worldCoords, view);
+        sprite.Position = TransformVectorToIsometric(gridCoords);
+        window.Draw(sprite);
+    }
+    public static void CreateBlock(Vector2f position, Block.Type type)
+    {
+        int x = (int)position.X;
+        int y = (int)position.Y;
+        if (x < 0 || y < 0 || x > 31 || y > 31)
+        {
+            return;
+        }
+        blocks[x, y, chosenElevation] = new Block(x, y, chosenElevation);
+        Block block = blocks[x, y, chosenElevation];
+        block.sprite = new Sprite(cubeText);
+        block.sprite.TextureRect = spritePos[type];
+        block.sprite.Position = TransformVectorToIsometric(position);
+    }
+    public static void RemoveBlock(Vector2f position)
+    {
+        int x = (int)position.X;
+        int y = (int)position.Y;
+        if (x < 0 || y < 0 || x > 31 || y > 31)
+        {
+            return;
+        }
+        blocks[x, y, chosenElevation] = null;
     }
     public static void KeyPressed()
     {
@@ -101,58 +160,78 @@ class Program
         {
             Vector2i mousePos = Mouse.GetPosition(window);
             Vector2f worldCoords = window.MapPixelToCoords(mousePos, view);
-            CreateBlock(GetMouseGrid(worldCoords, view, GetZoomFactor(view)));
+            CreateBlock(GetMouseGrid(worldCoords, view), Block.Type.Block);
+        }
+        if (Mouse.IsButtonPressed(Mouse.Button.Right))
+        {
+            Vector2i mousePos = Mouse.GetPosition(window);
+            Vector2f worldCoords = window.MapPixelToCoords(mousePos, view);
+
+            RemoveBlock(GetMouseGrid(worldCoords, view));
         }
     }
-    static float GetZoomFactor(View view)
+    public static void KeyPressedDelay()
     {
-        Vector2f size = view.Size;
-        float zoomFactor = WIDTH / size.X;
-
-        return zoomFactor;
+        if (Keyboard.IsKeyPressed(Keyboard.Key.Add))
+        {
+            if (chosenElevation < 7)
+            {
+                chosenElevation++;
+            }
+        }
+        if (Keyboard.IsKeyPressed(Keyboard.Key.Subtract))
+        {
+            if (chosenElevation > 0)
+            {
+                chosenElevation--;
+            }
+        }
     }
-    public static Vector2f TransformVector(Vector2f vector)
+    public static Vector2f TransformVectorToIsometric(Vector2f vector)
     {
         float x = vector.X;
         float y = vector.Y;
-        float newx = (x * 16) + (y * -16);
+        float newx = ((x * 16) + (y * -16)) + WIDTH / 2 - 16;
         float newy = (x * 8) + (y * 8);
 
         return new Vector2f(newx, newy);
     }
-    public static Vector2f GetMouseGrid(Vector2f inputVector, View view, float zoom)
+    public static Vector2f GetMouseGrid(Vector2f inputVector, View view)
     {
-        float x = inputVector.X / zoom;
-        float y = inputVector.Y / zoom;
-        //float x = inputVector.X;
-        //float y = inputVector.Y;
+        float x = inputVector.X - WIDTH / 2;
+        float y = inputVector.Y;
         float newx = ((x / 32) + (y / 16));
         float newy = (y / 16 - x / 32);
-        newx -= view.Center.X / 32;
-        newy -= view.Center.Y / 32;
-        float gridx = (float)Math.Floor(newx + 0.5f);
-        float gridy = (float)Math.Floor(newy + 0.5f);
+        float gridx = (float)Math.Floor(newx);
+        float gridy = (float)Math.Floor(newy);
         return new Vector2f(gridx, gridy);
     }
 }
 
-class Tile
+class Block
 {
     public int x = 0;
     public int y = 0;
+    public int z = 0;
     public float drawx = 0;
     public float drawy = 0;
     public Sprite sprite = new Sprite(Program.cubeText);
+    public enum Type
+    {
+        Block,
+        Water
+    }
 
     public Vector2f AddSinOffsetToSprite(float offset)
     {
         float posy = sprite.Position.Y;
         return new Vector2f(sprite.Position.X, sprite.Position.Y + offset);
     }
-    public Tile(int x, int y)
+    public Block(int x, int y, int z)
     {
         this.x = x;
         this.y = y;
+        this.z = z;
         sprite.TextureRect = new IntRect(0, 0, 32, 32);
     }
 }
