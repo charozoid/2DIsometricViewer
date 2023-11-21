00570e6983eb679ea66e82617981e0f7c0ce28ff
charozoid <directfromquebec@gmail.com>
2023-11-20 19:40:15 -0500

    Added cube drawing, can't figure out alignment with isometric grid

--- 2DIsometricViewer/Program.cs
+++ 2DIsometricViewer/Program.cs
@@ -10,12 +10,14 @@ class Program
     public static Texture cubeText = new Texture("../../Assets/spritesheet.png");
     public static Tile[,] tiles = new Tile[32,32];
     public static View view = new View(new FloatRect(0, 0, WIDTH, HEIGHT));
+    public static Font font = new Font("../../Assets/Fonts/arialbd.ttf");
     public static float CameraSpeed = 10.0f;
     public static float ZoomFactor = 1.05f;
     public static float amplitude = 0.25f;
     public static float frequency = 2f;
     public static Random random = new Random();
     public static Clock clock = new Clock();
+    public static List<Sprite> sprites = new List<Sprite>();
     public static void Main(string[] args)
     {
 
@@ -27,25 +29,47 @@ class Program
             window.SetView(view);
             window.Clear();
             DrawGrid();
-            KeyPressed();
 
+            Sprite sprite = new Sprite(cubeText);
+            sprite.TextureRect = new IntRect(0, 0, 32, 32);
+            Vector2i mousePos = Mouse.GetPosition(window);
+            Vector2f worldCoords = window.MapPixelToCoords(mousePos, view);
+            Vector2f gridCoords = GetMouseGrid(worldCoords, view, GetZoomFactor(view));
+            //Vector2f mousePosF = GetMouseGrid(new Vector2f(mousePos.X, mousePos.Y), view, GetZoomFactor(view));
+            sprite.Position = TransformVector(gridCoords);
+
+            Text text = new Text($"X: {gridCoords.X} Y: {gridCoords.Y}", font);
+            text.Position = new Vector2f(15, 50);
+            window.Draw(text);
+            KeyPressed();
+            foreach (Sprite tile in sprites)
+            {
+                window.Draw(tile);
+            }
+            window.Draw(sprite);
             window.DispatchEvents();
             window.Display();
         }
     }
     public static void DrawGrid()
     {
-        int offset = 512;
         Sprite sprite = new Sprite(cubeText);
-        sprite.TextureRect = new IntRect(64, 0, 32, 32);
+        sprite.TextureRect = new IntRect(128, 32, 32, 32);
         for (int i = 0; i < 32; i++)
         {
             for (int j = 0; j < 32; j++)
             {
-                sprite.Position = TransformVector(i, j);
+                sprite.Position = TransformVector(new Vector2f(i + 32, j));                
                 window.Draw(sprite);
             }
         }
+    }
+    public static void CreateBlock(Vector2f position)
+    {
+        Sprite sprite = new Sprite(cubeText);
+        sprite.TextureRect = new IntRect(0, 0, 32, 32);
+        sprite.Position = TransformVector(position);
+        sprites.Add(sprite);
     }
     public static void KeyPressed()
     {
@@ -73,14 +97,42 @@ class Program
         {
             view.Zoom(1.0f / ZoomFactor);
         }
+        if (Mouse.IsButtonPressed(Mouse.Button.Left))
+        {
+            Vector2i mousePos = Mouse.GetPosition(window);
+            Vector2f worldCoords = window.MapPixelToCoords(mousePos, view);
+            CreateBlock(GetMouseGrid(worldCoords, view, GetZoomFactor(view)));
+        }
     }
-
-    public static Vector2f TransformVector(int x, int y)
+    static float GetZoomFactor(View view)
     {
+        Vector2f size = view.Size;
+        float zoomFactor = WIDTH / size.X;
+
+        return zoomFactor;
+    }
+    public static Vector2f TransformVector(Vector2f vector)
+    {
+        float x = vector.X;
+        float y = vector.Y;
         float newx = (x * 16) + (y * -16);
         float newy = (x * 8) + (y * 8);
 
-        return new Vector2f(newx + 496, newy);
+        return new Vector2f(newx, newy);
+    }
+    public static Vector2f GetMouseGrid(Vector2f inputVector, View view, float zoom)
+    {
+        float x = inputVector.X / zoom;
+        float y = inputVector.Y / zoom;
+        //float x = inputVector.X;
+        //float y = inputVector.Y;
+        float newx = ((x / 32) + (y / 16));
+        float newy = (y / 16 - x / 32);
+        newx -= view.Center.X / 32;
+        newy -= view.Center.Y / 32;
+        float gridx = (float)Math.Floor(newx + 0.5f);
+        float gridy = (float)Math.Floor(newy + 0.5f);
+        return new Vector2f(gridx, gridy);
     }
 }
 
